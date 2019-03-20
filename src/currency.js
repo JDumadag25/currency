b3c86a21e45b0d0aea6201f7dacc0d27af4fe343
JDumadag25 <justin.dumadag@gmail.com>
2019-03-20 03:05:01 -0400

    just saving had to redo

--- src/currency.js
+++ src/currency.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+  100 cents = $1
+  let cents = 100
+  let coins = [25, 10, 5, 1]
+  let combinations = {"Quarter":0, "Dime":0, "Nickle":0, "Penny":0}
+
   function coinCounter() {
     console.log("hello");
   }
 
-  coinCounter()
+
+
 })
