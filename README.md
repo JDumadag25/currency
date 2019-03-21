40b8a058235c4c4a9f6bbfa6209978088918e80a
JDumadag25 <justin.dumadag@gmail.com>
2019-03-21 00:06:26 -0400

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Currency code challenge
+
+## Work In Progress
+
+Open the index.html webpage and go to the console to see the number of combinations of coins you can make with $1
--- src/currency.js
+++ src/currency.js
@@ -1,31 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
-  //
-  // let cents = 100
-  // let denominations = [25, 10 , 5, 1]
-  // let coinCombinations = []
-  //
-  // function coinCounter(amountLeft, i, combinations, addcoins){
-  //   if addcoins.push(addcoins)
-  //   if (amountLeft === 0 || (i+1) === denominations.length){
-  //     if(i+1 === denominations.length && amountLeft > 0){
-  //       combinations.push((amountLeft,denominations[i]))
-  //       i += 1
-  //     }
-  //     while (i < denominations.length){
-  //       i += 1
-  //       console.log(combinations);
-  //     }
-  //   }
-  //   let currentCoin = denominations[i]
-  //
-  // }
 
+  //
+  //----This function returns the count of coin combinations (242)----//
+  //
   // let cents = 100
   //
   // function coinCounter(total) {
   //   let count = 0
   //   let coins = [1,5,10,25]
-  //   let combinations = {"Quarter":0, "Dime":0, "Nickle":0, "Penny":0}
+  //
   //
   //   const coinCombinations = function(index,amountLeft){
   //
@@ -50,38 +33,37 @@ document.addEventListener("DOMContentLoaded", function(){
   // console.log(coinCounter(cents));
 
 
-  let cents = 100
+  //----This function I am currently working on to output all the possible combinations of coins----//
+
+  let cents = 10
 
   function coinCounter(total) {
     let count = 0
-    let coins = [1,5,10,25]
-    let coinObject = {"Quarter":0, "Dime":0, "Nickle":0, "Penny":0}
+    let coins = [1,5]
+    let coinObject = {"Quarter":0, "Dime":0, "Nickel":0, "Penny":0}
     let combinations = []
 
     const coinCombinations = function(index,amountLeft){
 
-      let currentCoin = coins[index];
-
       if(index === 0){
-        if(amountLeft % currentCoin === 0 ){
-          combinations.push([currentCoin, amountLeft])
-          console.log(combinations);
+        if(amountLeft % coins[index] === 0 ){
+          combinations.push([coins[index], amountLeft])
           count++
         }
         return
       }
+
       while(amountLeft >= 0){
-        coinCombinations(index-1, amountLeft)
-        combinations.push([currentCoin, 0])
-        //console.log(combinations);
-        amountLeft -= currentCoin
+          coinCombinations(index-1, amountLeft)
+          combinations.push([coins[index], amountLeft ]);
+          amountLeft -= coins[index];
       }
-      
     }
     coinCombinations(coins.length-1, total)
     return count
   }
 
 console.log(coinCounter(cents));
+
 
 })
