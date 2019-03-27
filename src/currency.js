deb72d9b1c7563ea0017429dc39ae46c8ff265b3
JDumadag25 <justin.dumadag@gmail.com>
2019-03-27 16:13:42 -0400

    trying out DP

--- src/currency.js
+++ src/currency.js
@@ -35,35 +35,46 @@ document.addEventListener("DOMContentLoaded", function(){
 
   //----This function I am currently working on to output all the possible combinations of coins----//
 
-  let cents = 10
+//   let cents = 10
+//
+//   function coinCounter(total) {
+//     let count = 0
+//     let coins = [1,5]
+//     let coinObject = {"Quarter":0, "Dime":0, "Nickel":0, "Penny":0}
+//     let combinations = []
+//
+//     const coinCombinations = function(index,amountLeft){
+//
+//       if(index === 0){
+//         if(amountLeft % coins[index] === 0 ){
+//           console.log(index);
+//           console.log(10%3);
+//           count++
+//         }
+//         return
+//       }
+//
+//       while(amountLeft >= 0){
+//         console.log(index);
+//           coinCombinations(index-1, amountLeft)
+//           //combinations.push([coins[index], amountLeft ]);
+//           amountLeft -= coins[index];
+//       }
+//     }
+//     coinCombinations(coins.length-1, total)
+//     return count
+//   }
+//
+// console.log(coinCounter(cents));
 
-  function coinCounter(total) {
-    let count = 0
-    let coins = [1,5]
-    let coinObject = {"Quarter":0, "Dime":0, "Nickel":0, "Penny":0}
-    let combinations = []
 
-    const coinCombinations = function(index,amountLeft){
+// Dynamic programming try
 
-      if(index === 0){
-        if(amountLeft % coins[index] === 0 ){
-          combinations.push([coins[index], amountLeft])
-          count++
-        }
-        return
-      }
-
-      while(amountLeft >= 0){
-          coinCombinations(index-1, amountLeft)
-          combinations.push([coins[index], amountLeft ]);
-          amountLeft -= coins[index];
-      }
-    }
-    coinCombinations(coins.length-1, total)
-    return count
-  }
-
-console.log(coinCounter(cents));
+function coinChange(amount, denominations){
+  let combinations = []
+  combinations[0] = 1
+  for(coin in denominations)
+}
 
 
 
